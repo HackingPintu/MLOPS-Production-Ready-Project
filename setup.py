f6601dbe057b7ab82479dfad7807c3eb4a2734a0
HackingPintu <rathpintu90@gmail.com>
2024-10-02 22:00:09 +0530

    Setup.py changed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rate_prediction",
+    version="0.0.0",
+    author="Rakesh",
+    author_email="rathpintu@gmail.com",
+    packages=find_packages()
+)
